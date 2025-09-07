198c8b6989cd1782d27e1ae7fd07b5dad2f213fa
tomisile <tomioluwasile@gmail.com>
2025-09-07 09:54:50 +0100

    fix: save and commit results.csv during workflow run

--- today.py
+++ today.py
@@ -1,14 +1,17 @@
 """Pull upcoming event data and analyse stats."""
 
 
+import os
 from utils import scrape_sb_today, save_to_csv
 
 
 # Scrape fresh data
 matches_data = scrape_sb_today()
 
+
 # Save to csv file
-events_today = save_to_csv(matches_data, "today.csv")
+csv_file = os.getenv('RESULT_LOG_FILE', 'today.csv')
+events_today = save_to_csv(matches_data, csv_file)
 
 # Save watchlist events to separate csv file
 # watchlist_events = save_to_csv(matches_data, "watchlist_today.csv")
