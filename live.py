432bf88a140080c1d5f9515778500c2499a27e67
tomisile <tomioluwasile@gmail.com>
2025-09-07 05:31:18 +0100

    refactor: remove unused prompt generator

--- live.py
+++ live.py
@@ -2,7 +2,7 @@
 
 
 from datetime import datetime
-from utils import scrape_sb_live, update_alert_log, generate_stats_prompts
+from utils import scrape_sb_live, update_alert_log
 
 
 # Scrape fresh data
@@ -10,7 +10,3 @@ matches_data = scrape_sb_live()
 
 # Save to file
 update_alert_log(matches_data)
-
-# Generate stats prompt
-stats_prompt = generate_stats_prompts(
-    matches_data, output_file='stats_prompt.txt')
